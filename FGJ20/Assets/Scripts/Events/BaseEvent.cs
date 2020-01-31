4b368bd91defc63a0182f0fe7bc6d8f22f374a93
Maru <markus.perakyla@gmail.com>
2020-02-01 01:39:04 +0200

    Character does the movement with nice animation now yes good.

---
+++ FGJ20/Assets/Scripts/Events/BaseEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEvent : MonoBehaviour
+{
+    public virtual void CallEvent()
+    {
+        Debug.Log("This is an event.");
+    }
+}
